827b1b7057a6f67cf91d717d7eaa26a383e5bbf3
John Doe <johndoe@example.com>
2025-04-19 18:06:02 +0200

    Refines button styles in MessageBubbleMenue component
    Improves button layout by ensuring full-width and consistent styling for edit and delete actions.
    
    Updates hover effects for better user interaction.
    
    No functional changes introduced.

--- components/ui/MessageBubble/MessageBubbleMenue.tsx
+++ components/ui/MessageBubble/MessageBubbleMenue.tsx
@@ -32,12 +32,12 @@ export default function MessageMenue({
         left: `${position.x}px`,
         top: `${position.y}px`,
       }}
-      className={`bg-white shadow-lg rounded-lg p-2 z-10 transform transition-all duration-200 ease-out `} // Animation classes
+      className={`bg-white shadow-lg rounded-lg p-2 z-10 transform transition-all duration-200 ease-out `}
     >
       <div className="flex-col">
         <button
           onClick={handleEdit}
-          className="text-sm flex items-center justify-center gap-2 p-2 hover:bg-gray-100 rounded-full transition-colors"
+          className="w-full cursor-pointer text-sm flex items-center  gap-2 p-2 hover:bg-gray-100 rounded-b-sm"
           title="Edit message"
         >
           <Pencil1Icon className="w-4 h-4 text-gray-600" />
@@ -45,7 +45,7 @@ export default function MessageMenue({
         </button>
         <button
           onClick={handleDelete}
-          className="text-sm  text-red-600 flex items-center justify-center gap-2  p-2 hover:bg-gray-100 rounded-full transition-colors"
+          className="w-full cursor-pointer text-sm  text-red-600 flex items-center gap-2  p-2 hover:bg-gray-100 ounded-b-sm transition-colors"
           title="Delete message"
         >
           <TrashIcon className="w-4 h-4 " />
