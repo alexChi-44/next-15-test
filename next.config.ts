d4369993ad098cd6e9b213513c35a81bb54baaf7
Alex <chuchanouski44@gmail.com>
2025-04-19 16:50:39 +0300

    next config modufy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ["localhost"],
   },
 };
 
