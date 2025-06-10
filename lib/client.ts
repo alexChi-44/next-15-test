172ba44ab1f533ed3fc328e61e69aed67687e800
Aliaksandr Chuchanouski <aliaksandr.chuchanouski@innowise.com>
2025-06-10 10:32:10 +0200

    added api and types

---
+++ lib/client.ts
@@ -0,0 +1,112 @@
+import { cookies } from "next/headers";
+import { IResponse } from "./types";
+
+export const getAuthTokenAction = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get(process.env.AUTH_TOKEN_COOKIE || "auth_token");
+  if (token) {
+    return `${token.name}=${token.value}`;
+  } else {
+    return false;
+  }
+};
+
+
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://api.lcb.loc";
+const API_BASE_VERSION = process.env.NEXT_PUBLIC_API_VERSION || "/api/v1/rest";
+
+type RequestOptions = RequestInit & {
+  isFormData?: boolean;
+};
+
+interface ApiResponse {
+  data?: unknown;
+  messages?: string[];
+  errors?: string[];
+  status?: number;
+}
+
+async function getAuthToken() {
+  if (typeof window !== "undefined") {
+    return false;
+  } else {
+    return await getAuthTokenAction();
+  }
+}
+
+async function request(endpoint: string, options: RequestOptions = {}) {
+  const { headers, isFormData, ...restOptions } = options;
+  const authCookie = await getAuthToken();
+  const requestHeaders = new Headers({
+    ...headers,
+    Accept: "application/json",
+  });
+
+  if (!isFormData) {
+    requestHeaders.append("Content-Type", "application/json");
+  }
+
+  if (authCookie) {
+    requestHeaders.append("Cookie", authCookie);
+  }
+
+  const request = new Request(`${API_BASE_URL}${API_BASE_VERSION}${endpoint}`, {
+    ...restOptions,
+    headers: requestHeaders,
+    credentials: "include",
+  });
+
+  const response = await fetch(request);
+  const data = await response.json();
+
+  return { ...data, ok: response.ok, status: response.status };
+}
+
+export const GET = (endpoint: string, options?: RequestOptions) =>
+  request(endpoint, { ...options, method: "GET" });
+
+export const POST = (
+  endpoint: string,
+  body?: unknown,
+  options?: RequestOptions,
+) =>
+  request(endpoint, {
+    ...options,
+    method: "POST",
+    body: body instanceof FormData ? body : JSON.stringify(body),
+  });
+
+export const PUT = (
+  endpoint: string,
+  body?: unknown,
+  options?: RequestOptions,
+) =>
+  request(endpoint, {
+    ...options,
+    method: "PUT",
+    body: body instanceof FormData ? body : JSON.stringify(body),
+  });
+
+export const PATCH = (
+  endpoint: string,
+  body?: unknown,
+  options?: RequestOptions,
+) =>
+  request(endpoint, {
+    ...options,
+    method: "PATCH",
+    body: body instanceof FormData ? body : JSON.stringify(body),
+  });
+
+export const DELETE = (
+  endpoint: string,
+  body?: unknown,
+  options?: RequestOptions,
+) =>
+  request(endpoint, {
+    ...options,
+    method: "DELETE",
+    body:
+      body instanceof FormData ? body : body ? JSON.stringify(body) : undefined,
+  });
